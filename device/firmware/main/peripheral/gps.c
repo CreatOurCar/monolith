408e1b1269be6ec18fdb5a2a3542f542d2a7317c
오병준 <mail@luftaquila.io>
2025-08-06 13:57:25 +0900

    impl: neo6m config cmd

--- device/firmware/main/peripheral/gps.c
+++ device/firmware/main/peripheral/gps.c
@@ -3,11 +3,9 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
 
-/*******************************************************************************
- * GPS NMEA GPRMC message monitor task
- ******************************************************************************/
-void task_gps(void *pvParameters) {
-  QueueHandle_t uart_queue;
+QueueHandle_t uart_queue;
+
+void init_ublox(void) {
   uart_config_t uart_config = {
     .baud_rate = 9600,
     .data_bits = UART_DATA_8_BITS,
@@ -23,6 +21,48 @@ void task_gps(void *pvParameters) {
     ERROR_SYSLOG(&init, GPS, "UART driver init failure", "GPS_UART_FAIL");
   }
 
+  const uint8_t GPS_DISABLE_NMEA_GxGGA[] = { 0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x01, 0x00, 0x24 };
+  const uint8_t GPS_DISABLE_NMEA_GxGLL[] = { 0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x01, 0x01, 0x2B };
+  const uint8_t GPS_DISABLE_NMEA_GxGSA[] = { 0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x02, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x01, 0x02, 0x32 };
+  const uint8_t GPS_DISABLE_NMEA_GxGSV[] = { 0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x03, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x01, 0x03, 0x39 };
+  const uint8_t GPS_DISABLE_NMEA_GxVTG[] = { 0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x05, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x01, 0x05, 0x47 };
+
+  const uint8_t GPS_PMS_FULL[]  = { 0xB5, 0x62, 0x06, 0x86, 0x00, 0x00, 0x8C, 0xAA };
+  const uint8_t GPS_RATE_10HZ[] = { 0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0x64, 0x00, 0x01, 0x00, 0x01, 0x00, 0x7A,
+    0x12 };
+
+  if (uart_write_bytes(UART_NUM_1, GPS_DISABLE_NMEA_GxGGA, sizeof(GPS_DISABLE_NMEA_GxGGA)) < 0 ||
+      uart_write_bytes(UART_NUM_1, GPS_DISABLE_NMEA_GxGLL, sizeof(GPS_DISABLE_NMEA_GxGLL)) < 0 ||
+      uart_write_bytes(UART_NUM_1, GPS_DISABLE_NMEA_GxGSA, sizeof(GPS_DISABLE_NMEA_GxGSA)) < 0 ||
+      uart_write_bytes(UART_NUM_1, GPS_DISABLE_NMEA_GxGSV, sizeof(GPS_DISABLE_NMEA_GxGSV)) < 0 ||
+      uart_write_bytes(UART_NUM_1, GPS_DISABLE_NMEA_GxVTG, sizeof(GPS_DISABLE_NMEA_GxVTG)) < 0 ||
+      uart_write_bytes(UART_NUM_1, GPS_PMS_FULL, sizeof(GPS_PMS_FULL)) < 0 ||
+      uart_write_bytes(UART_NUM_1, GPS_RATE_10HZ, sizeof(GPS_RATE_10HZ)) < 0) {
+    ERROR_SYSLOG(&init, GPS, "module config failure", "GPS_CFG_FAIL");
+  }
+
+  uart_flush_input(UART_NUM_1);
+}
+
+/*******************************************************************************
+ * GPS NMEA GPRMC message monitor task
+ ******************************************************************************/
+void task_gps(void *pvParameters) {
+  switch (storage.gps.dev) {
+    case GPS_DEV_UBLOX:
+      init_ublox();
+      break;
+
+    default:
+      ERROR_SYSLOG(&init, GPS, "unknown device", "GPS_DEV_UNKNOWN");
+      break;
+  }
+
   if (IS_OK(&init, GPS)) {
     CLEAR_ALL(&logbuf.run, GPS);
     SYSLOG("GPS_RDY");
@@ -30,32 +70,36 @@ void task_gps(void *pvParameters) {
     COPY_STATE(&logbuf.run, &init, GPS);
   }
 
-  while (true) {
-    uart_event_t event;
+  uint8_t data[256];
+  uart_event_t event;
 
+  while (true) {
     if (xQueueReceive(uart_queue, &event, portMAX_DELAY) != pdTRUE) {
       continue;
     }
 
-    uint8_t data[1024];
-
-    if (event.type == UART_PATTERN_DET) {
-      int pos = uart_pattern_pop_pos(UART_NUM_1);
-
-      if (pos < 0) {
-        uart_flush_input(UART_NUM_1);
-        continue;
-      }
-
-      int len = uart_read_bytes(UART_NUM_1, data, pos + 1, 50);
-
-      if (len < 0) {
-        uart_flush_input(UART_NUM_1);
-        continue;
-      }
-
-      printf("%.*s", len, data);
-      uart_pattern_queue_reset(UART_NUM_1, 16);
+    if (event.type != UART_PATTERN_DET) {
+      continue;
     }
+
+    int pos = uart_pattern_pop_pos(UART_NUM_1);
+
+    if (pos < 0) {
+      uart_flush_input(UART_NUM_1);
+      continue;
+    }
+
+    int len = uart_read_bytes(UART_NUM_1, data, pos + 1, 50);
+
+    if (len < 0) {
+      uart_flush_input(UART_NUM_1);
+      continue;
+    }
+
+    if (strncmp((char *)data, "$GPRMC", 6) == 0) {
+      printf("%.*s", len, data);
+    }
+
+    uart_pattern_queue_reset(UART_NUM_1, 16);
   }
 }
